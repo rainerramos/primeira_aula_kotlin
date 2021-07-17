2da811bf19626c0c122f5bea13cc378ed50f8d5d
Rainer Ramos <rainermr89@hotmail.com>
2021-07-17 16:12:22 -0300

    Controle de fluxo - While

---
+++ src/main/kotlin/flowWhile.kt
@@ -0,0 +1,16 @@
+fun main(){
+
+    var index: Int = 0
+    while (index < 100){
+        println(index)
+        index++
+
+        if (index == 50) {
+            break
+        }
+
+    }
+
+
+
+}
