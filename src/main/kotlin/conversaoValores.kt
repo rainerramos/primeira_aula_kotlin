b1f35661cfc97ae1216c9c73b2e52ec0e00beb09
Rainer Ramos <rainermr89@hotmail.com>
2021-07-17 16:42:42 -0300

    Exceções - Try, Catch e Finally

--- src/main/kotlin/conversaoValores.kt
+++ src/main/kotlin/conversaoValores.kt
@@ -1,31 +1,17 @@
-/**
-Double 64
-Float 32
-Long 64
-Int 32
-Short 16
-Byte 8
+import java.lang.NumberFormatException
 
-**/
+fun main() {
 
+    try {
+        println("shsh".toInt())
+    } catch (e: NumberFormatException) {
+        println("Esse valor não é um número!")
+    } catch (e: Exception) {
+        println("Algo de errado ocorreu")
+    } finally {
+        println("akdighaikd")
+        println("akdighaikd")
+    }
 
-fun teste (n1: Int){
-
-}
-
-
-fun main (){
-
-   val n1: Double = Double.MAX_VALUE
-    val b1: Byte = 100
-
-    println(b1.toShort())
-    println(b1.toLong())
-    println(b1.toFloat())
-    println(b1.toDouble())
-
-   println("19".toInt())
-    println("19".toFloat())
-    println("19".toDouble())
 
 }
