98ffbed164a6b7fdab2e63f93b4ead7c371961fb
Rainer Ramos <rainermr89@hotmail.com>
2021-07-17 16:02:39 -0300

    Controle de fluxo - For

---
+++ src/main/kotlin/flowFor.kt
@@ -0,0 +1,17 @@
+fun main (){
+
+    val str = "Rainer Ramos"
+    for (r in str) {
+        //print("$r ")
+    }
+
+    for (i in 0..100 step 5) {
+        if (i != 80) {
+            //print("$i ")
+        }
+    }
+
+    for (j in 100 downTo 0) {
+        print("$j ")
+    }
+}
