c0325a836c2e0bc0f87a2105aadd0b2e4491bdea
Rainer Ramos <rainermr89@hotmail.com>
2021-07-17 16:19:13 -0300

    Controle de fluxo - Do While

--- src/main/kotlin/flowWhile.kt
+++ src/main/kotlin/flowWhile.kt
@@ -1,16 +1,10 @@
 fun main(){
 
-    var index: Int = 0
-    while (index < 100){
-        println(index)
-        index++
 
-        if (index == 50) {
-            break
-        }
-
-    }
-
-
+    var l: Int = 10
+    do {
+        println(l)
+        l += 5
+    } while (l <= 100)
 
 }
