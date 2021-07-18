26a7782f574e3b2e76ad2edcaa7c8a602d203d93
Rainer Ramos <rainermr89@hotmail.com>
2021-07-18 15:42:25 -0300

    Sobre parâmetros

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,9 +1,12 @@
+
+fun endereco(rua: String = "", cidade: String, estado: String, cep: String, numero: Int = 0){
+    println("Rua= $rua")
+    println("Cidade: $cidade, $estado - $cep")
+}
+
+
 fun main() {
-
-    val str: String? = null
-
-    println(str?.length)
-
-    println(str!!.length)
-
+    endereco(cidade = "Campinas", estado = "São Paulo", cep = "13")
+    //endereco(cidade = "13", estado = "Campinas", cep = "São Paulo")
+    //endereco(cidade = "Campinas", estado = "São Paulo", cep = "13")
 }
